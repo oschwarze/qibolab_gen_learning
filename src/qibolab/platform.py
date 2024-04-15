4eb7fd6005ae66eded97d4c84d3016b9c84a355b
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-04-15 16:14:44 +0400

    chore: remove play_sequences

--- src/qibolab/platform.py
+++ src/qibolab/platform.py
@@ -242,18 +242,10 @@ class Platform:
         results = defaultdict(list)
         bounds = kwargs.get("bounds", self._controller.bounds)
         for b in batch(sequences, bounds):
-            try:
-                result = self._controller.play_sequences(
-                    self.qubits, self.couplers, b, options
-                )
-                for serial, values in result.items():
-                    results[serial].extend(values)
-
-            except NotImplementedError:
-                sequence, readouts = unroll_sequences(b, options.relaxation_time)
-                result = self._execute(sequence, options, **kwargs)
-                for serial, new_serials in readouts.items():
-                    results[serial].extend(result[ser] for ser in new_serials)
+            sequence, readouts = unroll_sequences(b, options.relaxation_time)
+            result = self._execute(sequence, options, **kwargs)
+            for serial, new_serials in readouts.items():
+                results[serial].extend(result[ser] for ser in new_serials)
 
         for serial, qubit in ro_pulses.items():
             results[qubit] = results[serial]
